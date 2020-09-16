a3b6563ac3867ae788211eccebf9486d256d2ea8
Alex Janiak (Mac) <alexander.janiak.acc@outlook.com>
2020-09-16 18:27:25 -0400

    added more UI elements

---
+++ Assets/Scripts/ImageExplorer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageExplorer : MonoBehaviour {
+    public Material material;
+    public Vector2 position;
+    public float scale;
+    float smoothScale;
+    Vector2 smoothPosition;
+
+    private void FixedUpdate() {
+        RescaleShader();
+        InputController();
+    }
+
+    void RescaleShader() {
+        smoothScale = Mathf.Lerp(smoothScale, scale, 0.5f);
+        smoothPosition = Vector2.Lerp(smoothPosition, position, 0.1f);
+
+        //float aspectRatio = (float)Screen.width / (float)Screen.height;
+        Rect imageRect = GetComponent<RectTransform>().rect;
+        float aspectRatio = imageRect.width / imageRect.height;
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+
+        if (aspectRatio > 1f) {
+            scaleY /= aspectRatio;
+        } else {
+            scaleX *= aspectRatio;
+        }
+        material.SetVector("_Area", new Vector4(smoothPosition.x, smoothPosition.y, scaleX, scaleY));
+    }
+
+    void InputController() {
+
+        //Controls zoom
+        if (Input.GetKey(KeyCode.Equals)) {
+            scale *= 0.99f;
+        }
+        if (Input.GetKey(KeyCode.Minus)) {
+            scale *= 1.01f;
+        }
+
+        //Controls origin position
+        if (Input.GetKey(KeyCode.A)) {
+            position.x -= 0.01f * scale;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            position.x += 0.01f * scale;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            position.y += 0.01f * scale;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            position.y -= 0.01f * scale;
+        }
+    }
+}
+
+//add higher percision double/decimal/BigInteger
+//add colors
+//add julia set alternatives (future itererations)
+//add tetration (explore it)
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UIController : MonoBehaviour
+{
+    public Material fractal;
+    public Shader julia;
+    public Shader mandelbrot;
+
+    public TMPro.TMP_InputField maxIterInputField;
+    public ToggleGroup shaderToggleGroup;
+
+    //Properties
+    int currentMaxIter;
+    int inputMaxIter {
+        get {
+            string text = maxIterInputField.text;
+            if (!text.Equals(string.Empty)) return int.Parse(maxIterInputField.text);
+            else return -1;
+        }
+    }
+
+    //Shader 
+    string currentShader;
+    string ActiveShaderName {
+        get { return shaderToggleGroup.ActiveToggles().FirstOrDefault().name; }
+    }
+
+    void Start() {
+        currentMaxIter = inputMaxIter;
+        currentShader = ActiveShaderName;
+    }
+
+    void Update() {
+        ManageProperties();
+        ToggleSelectedShader();
+    }
+
+    void ToggleSelectedShader() {
+        if(!currentShader.Equals(ActiveShaderName)) {
+            if (ActiveShaderName.Equals("MandelbrotToggle")) {
+                fractal.shader = mandelbrot;
+                currentShader = ActiveShaderName;
+            } else {
+                fractal.shader = julia;
+                currentShader = ActiveShaderName;
+            }
+        }
+    }
+
+    void ManageProperties() {
+        if (currentMaxIter != inputMaxIter && inputMaxIter > 0) {
+            fractal.SetInt("_MaxIter", inputMaxIter);
+            currentMaxIter = inputMaxIter;
+        }
+    }
+}
